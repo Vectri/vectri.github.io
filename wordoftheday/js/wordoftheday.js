337c390377e14d474333d949a2e7caf59148e0cd
Vectri <ctrlvectri@gmail.com>
2016-05-29 09:47:42 -0700

    Minify JS

--- wordoftheday/js/wordoftheday.js
+++ wordoftheday/js/wordoftheday.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+	'use strict';
  	var wordURL = "http://api.wordnik.com:80/v4/words.json/wordOfTheDay?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5&callback=?";
 	$.getJSON(wordURL)
 	.done(function (data) {
@@ -11,11 +12,11 @@ $(document).ready(function () {
 		}
 		var bghex = charToNumber(word, 0) + "" + charToNumber(word, 1)
 		+ "" + charToNumber(word, 2) + "" + charToNumber(word, 3);
-		while (bghex.length != 6) {
+		while (bghex.length !== 6) {
 			if (bghex.length > 6) {
 				bghex = bghex.slice(0, -1);
 			} else {
-				bghex += "0";				
+				bghex += "0";
 			}
 		}
 		bghex = Color_mixer.mix($.Color("#" + bghex), $.Color("#ffffff"))
@@ -28,4 +29,3 @@ $(document).ready(function () {
 		window.location = "http://vectri.github.io/";
 	})
 });
-
