6352924f15c3e8e347c6b014444d7a65b1c6a9d4
Vectri <ctrlvectri@gmail.com>
2016-06-02 20:50:28 -0700

    Fix location of HTTPS check

--- wordoftheday/js/wordoftheday.js
+++ wordoftheday/js/wordoftheday.js
@@ -7,10 +7,6 @@ $(document).ready(function () {
 		$("#word").text(word);
 		$("#partofspeech").text(data.definitions[0].partOfSpeech);
 		$("#definition").text(data.definitions[0].text);
-		if (location.protocol === 'https:') {
-			$("#word").text("API does not support HTTPS");	
-			$("#definition").text("If you want to view this page you must disable HTTPS.");
-		}
 		function charToNumber (s, i) {
 			return parseInt(s.charAt(i), 36) - 9;
 		}
@@ -31,5 +27,9 @@ $(document).ready(function () {
 	});
 	$(".author").click(function () {
 		window.location = "http://vectri.github.io/";
-	})
+	});
+	if (location.protocol === 'https:') {
+		$("#word").text("API does not support HTTPS");	
+		$("#definition").text("If you want to view this page you must disable HTTPS.");
+	}
 });
